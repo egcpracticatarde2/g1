0baf1650ffd195c7521fa1a7d42e1ce54b1327fa
agm1194 <auroragm94@gmail.com>
2016-12-29 14:19:40 +0100

    Volviendo a estado inicial

--- g1/src/main/Suma.java
+++ g1/src/main/Suma.java
@@ -2,8 +2,8 @@ package main;
 
 public class Suma {
 	
-	public Integer suma(Integer a, Integer b, Integer c){
-		return a + b + c;
+	public Integer suma(Integer a, Integer b){
+		return a + b;
 	}
 
 }
