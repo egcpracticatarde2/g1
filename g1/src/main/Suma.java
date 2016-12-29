ce8f10f66c511fb2549268dbff59b50f36c229ab
agm1194 <auroragm94@gmail.com>
2016-12-29 13:54:59 +0100

    Proyecto g1

---
+++ g1/src/main/Suma.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Suma {
+	
+	public Integer suma(Integer a, Integer b){
+		return a + b;
+	}
+
+}
---
+++ g1/src/test/TestSuma.java
@@ -0,0 +1,14 @@
+package test;
+
+import main.Suma;
+
+public class TestSuma {
+
+	public static void main(String[] args) {
+		Suma suma = new Suma();
+		Integer res = suma.suma(1, 2);
+		System.out.println(res);
+
+	}
+
+}
