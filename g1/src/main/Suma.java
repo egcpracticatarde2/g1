19d56c48e8c1d321643956636f67f5fa35319485
agm1194 <auroragm94@gmail.com>
2016-12-29 14:22:26 +0100

    Suma de 4 números

--- g1/src/main/Suma.java
+++ g1/src/main/Suma.java
@@ -2,8 +2,8 @@ package main;
 
 public class Suma {
 	
-	public Integer suma(Integer a, Integer b, Integer c){
-		return a + b + c;
+	public Integer suma(Integer a, Integer b, Integer c, Integer d){
+		return a + b + c + d;
 	}
 
 }
